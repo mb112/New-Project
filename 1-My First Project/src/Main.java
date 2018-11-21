96d416cc24dd49a6c452d80c6e2c26362f7d86f1
mustafabaykal112 <mustafabaykal112@gmail.com>
2018-11-21 13:28:17 -0500

    Java project

---
+++ 1-My First Project/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Mu name is Mike Smith");
+		System.out.print("As usual");
+		System.out.println("I am good");
+	}
+
+}
+
